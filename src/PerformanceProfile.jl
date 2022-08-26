53a475c262a1337936d8e98a26e355d97cdada1c
evcastelani <emersonvitor@gmail.com>
2022-08-26 15:31:09 -0300

    minor changes

--- src/PerformanceProfile.jl
+++ src/PerformanceProfile.jl
@@ -4,16 +4,17 @@ using Plots
 
 export perfomance_profile
 
-function perfomance_profile(c)
+function perfomance_profile(c, method = ["Alg $(i)" for i=1:size(c,2)], color = [:black,:gray])
     cmin = minimum(c,dims=2)
     R = c ./ cmin
     t = sort(unique(R))
-    if t[end] == Inf
+    if t[end] == NaN
         pop!(t)
     end
-    plot(xaxis=:log)
+    plot!(title = " ", xlabel = "Perfomance Ratio", ylabel = "Problems solved")
+    plot!(xaxis=:log)
     for i = 1:size(c, 2)
-        plot!(t, [sum(R[:,i] .<= ti)/size(c,1) for ti in t], label="Alg $i", t=:steppre, lw=2)
+        plot!(t, [sum(R[:,i] .<= ti)/size(c,1) for ti in t], label=method[i], t=:steppre, lw=2,linecolor = color[i])
     end
     ylims!(0, 1)
 end
