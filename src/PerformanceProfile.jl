b2908545c85b1737ff1266a0cd1ecc80fe53c12a
evcastelani <emersonvitor@gmail.com>
2022-08-31 09:13:49 -0300

    changes to test

--- src/PerformanceProfile.jl
+++ src/PerformanceProfile.jl
@@ -2,9 +2,9 @@ module PerformanceProfile
 
 using Plots
 
-export perfomance_profile
+export perfomance_profile, filtering
 
-function perfomance_profile(c, method = ["Alg $(i)" for i=1:size(c,2)], color = [:black,:gray])
+function perfomance_profile(c, method = ["Alg $(i)" for i=1:size(c,2)], color = [:black,:gray],style = [:dot,:solid])
     cmin = minimum(c,dims=2)
     R = c ./ cmin
     t = sort(unique(R))
@@ -12,10 +12,31 @@ function perfomance_profile(c, method = ["Alg $(i)" for i=1:size(c,2)], color = 
         pop!(t)
     end
     plot!(title = " ", xlabel = "Perfomance Ratio", ylabel = "Problems solved")
+    plot!(xticks=round.(0.0:0.5:3,digits=2))
     plot!(xaxis=:log)
     for i = 1:size(c, 2)
-        plot!(t, [sum(R[:,i] .<= ti)/size(c,1) for ti in t], label=method[i], t=:steppre, lw=2,linecolor = color[i])
+        plot!(t, [sum(R[:,i] .<= ti)/size(c,1) for ti in t], label=method[i], t=:steppre, lw=2,linecolor = color[i],line = style[i])
     end
     ylims!(0, 1)
+    savefig("tp_perf.pdf")
+end
+
+
+function filtering(c)
+    r = zeros(size(c))
+    k = 1
+    for i=1:length(c[:,1])
+        if c[i,1] == Inf && c[i,2] != Inf
+            println("problem $(i) were solved by 2 but does not 1")
+        elseif c[i,1] != Inf && c[i,2] == Inf
+            println("problem $(i) were solved by 1 but does not 2")
+        elseif c[i,1] == Inf && c[i,2] ==Inf
+            println("problem $(i) were not solved! This line was deleted")
+        else
+            r[k,:] = c[i,:]
+            k = k+1
+        end
+    end
+    return Float64.(r[1:k-1,:])
 end
 end # module
